329a088b04c9a078820406ca523fc0d258967f76
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2020-11-07 19:22:11 +0900

    Draw sample image

--- app/src/main/java/dev/mkeeda/day_night_wallpaper/service/DayNightWallpaperService.kt
+++ app/src/main/java/dev/mkeeda/day_night_wallpaper/service/DayNightWallpaperService.kt
@@ -1,9 +1,12 @@
 package dev.mkeeda.day_night_wallpaper.service
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Paint
 import android.service.wallpaper.WallpaperService
 import android.view.SurfaceHolder
+import dev.mkeeda.day_night_wallpaper.R
 
 class DayNightWallpaperService : WallpaperService() {
     override fun onCreateEngine(): Engine {
@@ -13,19 +16,36 @@ class DayNightWallpaperService : WallpaperService() {
     inner class DayNightWallpaperEngine : Engine() {
         override fun onSurfaceCreated(holder: SurfaceHolder?) {
             super.onSurfaceCreated(holder)
-            doDraw()
+            holder ?: return
+            drawImage(holder)
         }
 
-        private fun doDraw() {
-            val canvas = surfaceHolder.lockCanvas()
-            val x = (canvas.width / 2).toFloat()
-            val y = (canvas.height / 2).toFloat()
-            val paint = Paint()
+        private fun drawImage(holder: SurfaceHolder) {
+            val canvas = holder.lockCanvas()
+            val srcBitmap = BitmapFactory.decodeResource(resources, R.drawable.sample_image)
+
+            val originalWidth = srcBitmap.width
+            val originalHeight = srcBitmap.height
+            val (scaledWidth, scaledHeight) = when {
+                originalWidth > originalHeight -> {
+                    val ratio = originalHeight.toFloat() / originalWidth.toFloat()
+                    val scaledWidth = (canvas.width * ratio).toInt()
+                    scaledWidth to canvas.height
+                }
+                originalWidth < originalHeight -> {
+                    val ratio = originalWidth.toFloat() / originalHeight.toFloat()
+                    val scaledHeight = (canvas.height * ratio).toInt()
+                    canvas.width to scaledHeight
+                }
+                else -> {
+                    canvas.width to canvas.height
+                }
+            }
+            val scaledBitmap = Bitmap.createScaledBitmap(srcBitmap, scaledWidth, scaledHeight, true)
             canvas.drawColor(Color.BLACK)
-            paint.textSize = 56F
-            paint.color = Color.WHITE
-            canvas.drawText("Hello wallpaper!", x, y, paint)
-            surfaceHolder.unlockCanvasAndPost(canvas)
+            canvas.drawBitmap(scaledBitmap, 0F, 0F, Paint())
+
+            holder.unlockCanvasAndPost(canvas)
         }
     }
 }
