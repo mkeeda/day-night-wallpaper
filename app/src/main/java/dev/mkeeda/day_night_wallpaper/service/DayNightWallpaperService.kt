b7e7beed3678fcea0fa97a6d174d38947bc8c863
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2020-11-07 20:31:04 +0900

    Listen ui mode changes

--- app/src/main/java/dev/mkeeda/day_night_wallpaper/service/DayNightWallpaperService.kt
+++ app/src/main/java/dev/mkeeda/day_night_wallpaper/service/DayNightWallpaperService.kt
@@ -1,5 +1,6 @@
 package dev.mkeeda.day_night_wallpaper.service
 
+import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -7,17 +8,68 @@ import android.graphics.Paint
 import android.service.wallpaper.WallpaperService
 import android.view.SurfaceHolder
 import dev.mkeeda.day_night_wallpaper.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@ExperimentalCoroutinesApi
 class DayNightWallpaperService : WallpaperService() {
+    private val uiMode = MutableStateFlow(false)
+
     override fun onCreateEngine(): Engine {
         return DayNightWallpaperEngine()
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        println("⭐ configure changed")
+        uiMode.value = isDarkModeOn()
+    }
+
+    private fun isDarkModeOn(): Boolean {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
+    }
+
     inner class DayNightWallpaperEngine : Engine() {
+        private val engineJob = Job()
+        private val engineScope = CoroutineScope(Dispatchers.Main + engineJob)
+
         override fun onSurfaceCreated(holder: SurfaceHolder?) {
             super.onSurfaceCreated(holder)
             holder ?: return
             drawImage(holder)
+            println("⭐ surface created ${isDarkModeOn()}")
+            engineScope.launch {
+                uiMode.collect {
+                    println("⭐ collect ${isDarkModeOn()}")
+                }
+            }
+        }
+
+        override fun onSurfaceChanged(
+            holder: SurfaceHolder?,
+            format: Int,
+            width: Int,
+            height: Int
+        ) {
+            super.onSurfaceChanged(holder, format, width, height)
+            println("⭐ surface changed ${isDarkModeOn()}")
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            engineScope.cancel()
+        }
+
+        private fun isDarkModeOn(): Boolean {
+            val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            return currentNightMode == Configuration.UI_MODE_NIGHT_YES
         }
 
         private fun drawImage(holder: SurfaceHolder) {
