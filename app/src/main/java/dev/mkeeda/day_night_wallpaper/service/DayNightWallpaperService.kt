b914aa68d4f749de3390bfdb6389511f29796d2c
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2020-11-22 19:53:26 +0900

    Use repository in WallpaperService

--- app/src/main/java/dev/mkeeda/day_night_wallpaper/service/DayNightWallpaperService.kt
+++ app/src/main/java/dev/mkeeda/day_night_wallpaper/service/DayNightWallpaperService.kt
@@ -1,56 +1,84 @@
 package dev.mkeeda.day_night_wallpaper.service
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Paint
-import android.service.wallpaper.WallpaperService
+import android.net.Uri
 import android.view.SurfaceHolder
 import androidx.core.graphics.scale
-import dev.mkeeda.day_night_wallpaper.R
+import androidx.core.net.toUri
+import androidx.lifecycle.lifecycleScope
+import dev.mkeeda.day_night_wallpaper.data.WallpaperRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.withContext
 
-class DayNightWallpaperService : WallpaperService() {
-    override fun onCreateEngine(): Engine {
-        return DayNightWallpaperEngine()
+class DayNightWallpaperService : LifecycleWallpaperService() {
+    override fun onCreateEngineWithLifecycle(): Engine {
+        val wallpaperRepository = WallpaperRepository(context = this)
+        return DayNightWallpaperEngine(wallpaperRepository)
     }
 
-    inner class DayNightWallpaperEngine : Engine() {
+    inner class DayNightWallpaperEngine(
+        private val wallpaperRepository: WallpaperRepository
+    ) : Engine() {
         override fun onSurfaceCreated(holder: SurfaceHolder?) {
             super.onSurfaceCreated(holder)
             holder ?: return
-            drawImage(holder)
+
+            lifecycleScope.launchWhenStarted {
+                wallpaperRepository.wallpaperFlow
+                    .filterNotNull()
+                    .collect { wallpaper ->
+                        drawImage(
+                            holder = holder,
+                            uri = wallpaper.lightImageUri.toUri()
+                        )
+                    }
+            }
         }
 
-        private fun drawImage(holder: SurfaceHolder) {
-            val canvas = holder.lockCanvas()
-            val srcBitmap = BitmapFactory.decodeResource(resources, R.drawable.sample_image)
+        private suspend fun drawImage(holder: SurfaceHolder, uri: Uri) {
+            withContext(Dispatchers.IO) {
+                val canvas = holder.lockCanvas()
+                val srcBitmap = uri.loadBitmap()
 
-            // Scale to fit center of screen
-            val originalWidth = srcBitmap.width
-            val originalHeight = srcBitmap.height
-            val (scaledWidth, scaledHeight) = when {
-                originalWidth > originalHeight -> {
-                    val ratio = originalHeight.toFloat() / originalWidth.toFloat()
-                    val scaledWidth = (canvas.width * ratio).toInt()
-                    scaledWidth to canvas.height
+                // Scale to fit center of screen
+                val originalWidth = srcBitmap.width
+                val originalHeight = srcBitmap.height
+                val (scaledWidth, scaledHeight) = when {
+                    originalWidth > originalHeight -> {
+                        val ratio = originalHeight.toFloat() / originalWidth.toFloat()
+                        val scaledWidth = (canvas.width * ratio).toInt()
+                        scaledWidth to canvas.height
+                    }
+                    originalWidth < originalHeight -> {
+                        val ratio = originalWidth.toFloat() / originalHeight.toFloat()
+                        val scaledHeight = (canvas.height * ratio).toInt()
+                        canvas.width to scaledHeight
+                    }
+                    else -> {
+                        canvas.width to canvas.height
+                    }
                 }
-                originalWidth < originalHeight -> {
-                    val ratio = originalWidth.toFloat() / originalHeight.toFloat()
-                    val scaledHeight = (canvas.height * ratio).toInt()
-                    canvas.width to scaledHeight
-                }
-                else -> {
-                    canvas.width to canvas.height
-                }
+                val scaledBitmap = srcBitmap.scale(width = scaledWidth, height = scaledHeight)
+
+                val centerX = (canvas.width - scaledWidth) / 2.0f
+                val centerY = (canvas.height - scaledHeight) / 2.0f
+
+                canvas.drawColor(Color.BLACK)
+                canvas.drawBitmap(scaledBitmap, centerX, centerY, Paint())
+
+                holder.unlockCanvasAndPost(canvas)
             }
-            val scaledBitmap = srcBitmap.scale(width = scaledWidth, height = scaledHeight)
+        }
+    }
 
-            val centerX = (canvas.width - scaledWidth) / 2.0f
-            val centerY = (canvas.height - scaledHeight) / 2.0f
-
-            canvas.drawColor(Color.BLACK)
-            canvas.drawBitmap(scaledBitmap, centerX, centerY, Paint())
-
-            holder.unlockCanvasAndPost(canvas)
+    private fun Uri.loadBitmap(): Bitmap {
+        return contentResolver.openFileDescriptor(this, "r").use { parcelFileDescriptor ->
+            BitmapFactory.decodeFileDescriptor(parcelFileDescriptor?.fileDescriptor)
         }
     }
 }
